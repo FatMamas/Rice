dd307fd6c2a71bc2f33b39abe7b2553b81387e3f
Petr Bělohlávek <me@petrbel.cz>
2015-12-08 22:30:01 +0100

    loading last dump

--- main.py
+++ main.py
@@ -154,8 +154,8 @@ def iterate_minibatches(inputs, targets, batch_size, shuffle=False):
             excerpt = indices[start_idx:start_idx + batch_size]
         else:
             excerpt = slice(start_idx, start_idx + batch_size)
-        yield inputs[excerpt], targets[excerpt]
-        # yield inputs[1:5], targets[1:5] # TODO: do not use this! never!
+        # yield inputs[excerpt], targets[excerpt]
+        yield inputs[1:50], targets[1:50] # TODO: do not use this! never!
 
 
 def recall(recall_fn, patterns):
@@ -229,14 +229,14 @@ if __name__ == "__main__":
     target_var = T.ivector('targets')
 
     logging.info("Importing the network module")    # we need to import this AFTER Theano and Lasagne
-    # from model.mnist import build_network as build_network
+    from model.mnist import build_network as build_network
     # from model.official import build_cifar_network as build_network
     # from model.conv3 import build_network_3cc as build_network
     # from model.conv4 import build_network_4cc as build_network
     # from model.winner import build_network_winner as build_network
     # from model.tomas import build_network_tomas as build_network
     # from model.tomas2 import build_network_tomas2 as build_network
-    from model.tomas2_1 import build_network_tomas2_1 as build_network
+    # from model.tomas2_1 import build_network_tomas2_1 as build_network
 
     logging.info("Building the network")
     network, net_name = build_network(config, input_var)
@@ -275,6 +275,7 @@ if __name__ == "__main__":
     os.makedirs(config.output, exist_ok=True)
 
     best_acc = -1
+    last_save = 0
     with open('{}/{}.csv'.format(config.log, net_name), 'w') as log_f:
         log_f.write("epoch;trainloss;valloss;valacc\n")
 
@@ -311,6 +312,7 @@ if __name__ == "__main__":
             # save better model if it's better then the previous best one
             if val_acc / val_batches > best_acc:
                 best_acc = val_acc / val_batches
+                last_save = epoch
                 network_filename = "{}/network_{}_{}.dat".format(config.output, net_name, epoch)
                 logging.info("Saving the model into '%s'", network_filename)
                 save_network(network_filename, network, net_name, input_var, target_var, prediction, loss, params,
@@ -318,8 +320,8 @@ if __name__ == "__main__":
 
     logging.info("Training finished")
 
-    logging.info("Reading it just to be sure")
-    network_filename = "{}/network_{}_{}.dat".format(config.output, net_name, 0)
+    network_filename = "{}/network_{}_{}.dat".format(config.output, net_name, last_save)
+    logging.info("Reading it just to be sure: {}".format(network_filename))
     network_n, net_name_n, input_var_n, target_var_n, prediction_n, loss_n, params_n, updates_n, test_prediction_n, test_loss_n, predict_fn_n, test_acc_n, train_fn_n, val_fn_n = load_network(network_filename)
     logging.info("Printing 20 random classifications")
     test_random(predict_fn_n, test_data, test_labels, n=20)
